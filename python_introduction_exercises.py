4f2b3615e1779ba5e2f6c4cfb38c5e280ff23322
Ramiro-Lopez <lopez.j.ramiro1@gmail.com>
2023-08-03 20:35:44 -0500

    added f string

--- python_introduction_exercises.py
+++ python_introduction_exercises.py
@@ -1,6 +1,9 @@
-# this is my first python script 
+# this is my first python script
 print('Hello, World')
 
 # this is my first variable
 greeting = 'Hello, world'
 print(greeting)
+
+# this is an f string
+print(f'{greeting} I am heading out')
